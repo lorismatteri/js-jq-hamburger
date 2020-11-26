68d7699decd3afb78b3a1c0ea56dd461fd98407c
Loris Matteri <loris.matteri93@gmail.com>
2020-11-26 16:24:59 +0100

    JQ Apparizione/sparizione menu
    mostro e nascondo il menu al click sull'icona hamburger e sull'icona close

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("ciaone");
+//Mostrare e nascondere menu
+
+//1. Vado a prendere l'icona hamburger con jQuery e la metto in una variabile
+var hamMenu = $('.header-right > a');
+
+//2. Al click faccio aprire il menu
+hamMenu.click(function() {
+    $('.hamburger-menu').fadeIn(1000);
+});
+
+//3. Vado a predere l'icona "chiudi" con jQuery e la metto in una variabile
+
+var closure = $('.close');
+
+//4. Al click faccio chiudere il menu
+closure.click(function() {
+    $('.hamburger-menu').fadeOut(1000);
+});
